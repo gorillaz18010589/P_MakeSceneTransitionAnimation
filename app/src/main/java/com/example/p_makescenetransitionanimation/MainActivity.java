593a8aa91dae2770f7350e83e38f39471985b8b7
jennifer <gorillaz1801058@gmail.com>
2020-07-22 22:13:10 +0800

    Ｖ2 新增Slide,Explode,Fade轉場特效,新增文字匡縮放特效

--- app/src/main/java/com/example/p_makescenetransitionanimation/MainActivity.java
+++ app/src/main/java/com/example/p_makescenetransitionanimation/MainActivity.java
@@ -9,17 +9,22 @@ import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.transition.ChangeBounds;
+import android.transition.Slide;
+import android.transition.Transition;
 import android.util.Log;
 import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView imageView;
     private TextView tv1, tv2 ,tv3;
     private Button btnToPage2;
+    private LinearLayout lin1,lin2,lin3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,14 +43,20 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this,SecondActivity.class);
 
                 //3.準備ActivityOptions.makeSceneTransitionAnimation方法,製造綁定特效果
-                Pair[] pairs = new Pair[4];
+                Pair[] pairs = new Pair[7];
                 pairs[0]  = new Pair<View,String>(imageView,"transition1");
                 pairs[1]  = new Pair<View,String>(tv1,"transitio2");
                 pairs[2]  = new Pair<View,String>(tv2,"transitio3");
                 pairs[3]  = new Pair<View,String>(tv3,"transitio4");
+                pairs[4]  = new Pair<View,String>(lin1,"lin1");
+                pairs[5]  = new Pair<View,String>(lin2,"lin2");
+                pairs[6]  = new Pair<View,String>(lin3,"lin3");
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+//                    getWindow().setEnterTransition(new Slide().setDuration(001));
+//                    getWindow().setExitTransition(new Slide().setDuration(001));
                     ActivityOptions activityOptions = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,pairs);
                     startActivity(intent,activityOptions.toBundle());
+
                 }else{
                     Log.v("hank","小於的版本沒效果");
                 }
@@ -61,5 +72,18 @@ public class MainActivity extends AppCompatActivity {
         tv2 = findViewById(R.id.tv2);
         tv3 = findViewById(R.id.tv3);
         btnToPage2 = findViewById(R.id.btnToPage2);
+
+        lin1 = findViewById(R.id.lin1);
+        lin2 = findViewById(R.id.lin2);
+        lin3 = findViewById(R.id.lin3);
+    }
+
+    private Transition enterTransition() {
+        ChangeBounds bounds = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+            bounds = new ChangeBounds();
+            bounds.setDuration(2000);
+        }
+        return bounds;
     }
 }
--- app/src/main/java/com/example/p_makescenetransitionanimation/SecondActivity.java
+++ app/src/main/java/com/example/p_makescenetransitionanimation/SecondActivity.java
@@ -1,14 +1,50 @@
 package com.example.p_makescenetransitionanimation;
-
+//Window.setEnterTransition(Transition transition):轉場開始特效
+//Window.setExitTransition(Transition transition):轉場開始特效
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.os.Build;
 import android.os.Bundle;
+import android.transition.Explode;
+import android.transition.Fade;
+import android.transition.Slide;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
 
 public class SecondActivity extends AppCompatActivity {
-
+    private LinearLayout linCon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        linCon = findViewById(R.id.linCon);
+//        setScale();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+            //下拉
+//            getWindow().setEnterTransition(new Slide().setDuration(1000));
+//            getWindow().setExitTransition(new Slide().setDuration(001));
+
+            //分解
+//            getWindow().setEnterTransition(new Explode().setDuration(2000));
+//            getWindow().setExitTransition(new Explode().setDuration(2000));
+
+            //淡入淡出
+            getWindow().setEnterTransition(new Fade().setDuration(100));
+            getWindow().setExitTransition(new Fade().setDuration(100));
+
+
+        } else {
+
+        }
+    }
+
+    //設定容器縮放特效
+    private void setScale() {
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.scale);
+        linCon.setAnimation(animation);
     }
 }
